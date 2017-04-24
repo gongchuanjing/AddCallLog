ca301b847d004b3a429389b45b38fe77f1fca016
gcj <longgongsanri@163.com>
2017-04-24 13:45:33 +0800

    切换TimePickerView，之前用的TimePickerView在乐视手机上有问题

--- app/src/main/java/tk/chuanjing/addcalllog/activity/MainActivity.java
+++ app/src/main/java/tk/chuanjing/addcalllog/activity/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.airsaid.pickerviewlibrary.TimePickerView;
+import com.bigkoo.pickerview.TimePickerView;
 
 import java.util.Date;
 
@@ -118,23 +118,33 @@ public class MainActivity extends BaseActivity {
      * 选择通话开始时间
      */
     private void selectStartTime() {
-        TimePickerView mTimePickerView = new TimePickerView(this, TimePickerView.Type.ALL);
-        // 设置是否循环
-        mTimePickerView.setCyclic(true);
-        // 设置滚轮文字大小
-        // mTimePickerView.setTextSize(TimePickerView.TextSize.SMALL);
-        // 设置时间可选范围(结合 setTime 方法使用,必须在)
-        // Calendar calendar = Calendar.getInstance();
-        // mTimePickerView.setRange(calendar.get(Calendar.YEAR) - 100, calendar.get(Calendar.YEAR));
-        // 设置选中时间
-        // mTimePickerView.setTime(new Date());
-        mTimePickerView.setOnTimeSelectListener(new TimePickerView.OnTimeSelectListener() {
+        TimePickerView pvTime = new TimePickerView.Builder(this, new TimePickerView.OnTimeSelectListener() {
             @Override
-            public void onTimeSelect(Date date) {
+            public void onTimeSelect(Date date,View v) {//选中事件回调
                 callLogDate = date.getTime();
                 tv_start_time.setText(DateAndTimeUtils.getTimeForDate("yyyy-MM-dd HH:mm", date));
             }
-        });
-        mTimePickerView.show();
+        })
+                .setType(TimePickerView.Type.YEAR_MONTH_DAY_HOUR_MIN)
+//                .setCancelText("Cancel")//取消按钮文字
+//                .setSubmitText("Sure")//确认按钮文字
+//                .setContentSize(18)//滚轮文字大小
+//                .setTitleSize(20)//标题文字大小
+//                .setTitleText("Title")//标题文字
+                .setOutSideCancelable(false)//点击屏幕，点在控件外部范围时，是否取消显示
+                .isCyclic(true)//是否循环滚动
+//                .setTitleColor(Color.BLACK)//标题文字颜色
+//                .setSubmitColor(Color.BLUE)//确定按钮文字颜色
+//                .setCancelColor(Color.BLUE)//取消按钮文字颜色
+//                .setTitleBgColor(0xFF666666)//标题背景颜色 Night mode
+//                .setBgColor(0xFF333333)//滚轮背景颜色 Night mode
+//                .setRange(calendar.get(Calendar.YEAR) - 20, calendar.get(Calendar.YEAR) + 20)//默认是1900-2100年
+//                .setDate(selectedDate)// 如果不设置的话，默认是系统时间*/
+//                .setRangDate(startDate,endDate)//起始终止年月日设定
+//                .setLabel("年","月","日","时","分","秒")
+//                .isCenterLabel(false) //是否只显示中间选中项的label文字，false则每项item全部都带有label。
+//                .isDialog(true)//是否显示为对话框样式
+                .build();
+        pvTime.show();
     }
 }
